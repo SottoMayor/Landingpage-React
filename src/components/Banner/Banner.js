ffed108fe705166b9f150fa7d0f996b8e48e4d77
David Sotto Mayor <ddsmmf@gmail.com>
2021-03-14 10:17:19 -0400

    ending the Banner with Board component, creating WhiteScreen component

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,25 +4,38 @@ import classes from './Banner.module.css';
 
 import Board from '../UI/Board/Board';
 
+import WhiteScreen from '../UI/WhiteScreen/WhiteScreen'
+
 const Banner = () => {
+
+    const boardContent = [
+        {text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', id: 1 },
+        {text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', id: 2},
+        {text: 'Et harum quidem rerum facilis est et expedita distinctio.', id: 3}
+    ]
+
     return (
         <div className={classes.Banner}>
-            
+
             <div className={classes.CenterY}>
 
                 <Board>
                     <ul>
-                        <li>
-                        A expressão Lorem ipsum em design gráfico e editoração é um texto padrão em latim utilizado na produção gráfica para preencher os espaços de texto
-                        </li>
-                        <li>
-                        A expressão Lorem ipsum em design gráfico e editoração é um texto padrão em latim utilizado na produção gráfica para preencher os espaços de texto
-                        </li>
-                        <li>
-                        A expressão Lorem ipsum em design gráfico e editoração é um texto padrão em latim utilizado na produção gráfica para preencher os espaços de texto
-                        </li>   
-                    </ul>    
+                        {
+                            boardContent.map(item => (
+                                <li key={item.id}> {item.text} </li>
+                            ))
+                        }
+                    </ul>
                 </Board>
+                
+                <div className={classes.WSContainer}>
+
+                    <WhiteScreen>
+                            Hello!
+                    </WhiteScreen>
+
+                </div>
 
             </div>
 
---
+++ src/components/UI/WhiteScreen/WhiteScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './WhiteScreen.module.css';
+
+const WhiteScreen = (props) => {
+    return (
+        <div className={classes.WhiteScreen}>
+            {props.children}
+        </div>
+    )
+}
+
+export default WhiteScreen;
